ce71bfa3ee426313d8e4a0715bc5f4f773ac8a98
Rajeev Lochan <MacUser@rjs-macmini-08.local>
2015-12-11 14:18:37 +0530

    initial commit

---
+++ FailedBankCD/FailedBankInfo.m
@@ -0,0 +1,20 @@
+//
+//  FailedBankInfo.m
+//  FailedBankCD
+//
+//  Created by Mac User on 02/12/15.
+//  Copyright (c) 2015 Rajeev Lochan. All rights reserved.
+//
+
+#import "FailedBankInfo.h"
+#import "FailedBankDetails.h"
+
+
+@implementation FailedBankInfo
+
+@dynamic city;
+@dynamic name;
+@dynamic state;
+@dynamic details;
+
+@end
