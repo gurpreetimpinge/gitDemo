d656c66396eba559a21acaee5e7a0c860c58c236
MacUser <meghasehal@ganga.local>
2015-12-18 12:22:53 +0530

    Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by MacUser on 12/11/15.
+//  Copyright (c) 2015 Megha Sahal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by MacUser on 12/11/15.
+//  Copyright (c) 2015 Megha Sahal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
